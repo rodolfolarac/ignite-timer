867fcf5c0c60808141048abd0f33dfaa8ac0c316
Rodolfo <rodolfo.cassaro@auaha.net>
2023-08-09 09:41:57 -0300

    exemplo de theme provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { ThemeProvider } from "styled-components"
+import { defaultTheme } from "./styles/themes/default"
+
 import { Button } from "./components/Button"
 
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button/>
       <Button variant="success"/>
       <Button variant="danger"/>
-    </>
+    </ThemeProvider>
   )
 }
 
--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -17,7 +17,9 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
     height: 40px;
 
-    ${
+    background-color: ${props => props.theme.primary};
+
+    /* ${
         ({ variant }) => css`background-color: ${buttonVariants[variant]}`
-}
+} */
 `
