f4cc94992a0885060bf96467cb82677b1d8439b2
Rodolfo <rodolfo.cassaro@auaha.net>
2023-08-09 09:31:31 -0300

    exemplo de styled

---
+++ src/components/Button.tsx
@@ -0,0 +1,12 @@
+import { ButtonContainer } from "./Button.styles";
+
+import {ButtonVariant} from "./Button.styles"
+
+interface ButtonProps {
+    variant?: ButtonVariant;
+}
+
+
+export function Button({variant = "primary"} : ButtonProps) {
+    return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
+}
