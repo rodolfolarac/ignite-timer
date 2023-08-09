a51ccce4a798374f81424e50bd5fa936ad823211
Rodolfo <rodolfo.cassaro@auaha.net>
2023-08-09 16:53:17 -0300

    React router Dom inicializado na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,18 @@
 import { ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
 import { defaultTheme } from './styles/themes/default'
 
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
+import { Router } from './Router'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Button />
-      <Button variant="success" />
-      <Button variant="danger" />
+      <BrowserRouter>
+        <Router />
 
-      <GlobalStyle />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   )
 }
